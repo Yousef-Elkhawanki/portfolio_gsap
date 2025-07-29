a1129bde558287512e8efc7248433d3457ca5351
Yousef El-khawanki <dev.elkhawanki@gmail.com>
2025-07-29 12:12:19 +0300

    change tools card color

--- src/components/toolsCard/ToolsCard.tsx
+++ src/components/toolsCard/ToolsCard.tsx
@@ -5,7 +5,7 @@ interface IToolsCard {
 }
 export default function ToolsCard({ image, title }: IToolsCard) {
     return (
-        <div className="tools-card bg-[var(--dark-color)] p-4 rounded-2xl flex items-center gap-2 animated-text" >
+        <div className="tools-card bg-dark p-4 rounded-2xl flex items-center gap-2 animated-text" >
             <div className="tools-card-image w-14 h-14 bg-[var(--dark-secondary-color)] rounded-2xl flex items-center justify-center overflow-hidden">
                 <img src={image} alt={title} className="w-[70%] h-[70%] object-contain" />
             </div>
